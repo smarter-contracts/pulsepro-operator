62981a38b1bd8c05a4c73a1fb19596f8792202a6
Tim Clifford <tclifford@smartercontracts.co.uk>
2024-09-06 11:01:16 +0100

    Deployment controller

--- api/v1alpha1/pulseprodeployment_types.go
+++ api/v1alpha1/pulseprodeployment_types.go
@@ -1,47 +1,35 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// PulseProDeploymentSpec defines the desired state of PulseProDeployment
 type PulseProDeploymentSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of PulseProDeployment. Edit pulseprodeployment_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	HelmChartVersion    string             `json:"helmChartVersion"`
+	PulseProVersion     string             `json:"pulseProVersion"`
+	HelmValuesConfigMap ConfigMapReference `json:"helmValuesConfigMap"`
+	Secrets             []SecretReference  `json:"secrets"`
+	Namespace           string             `json:"namespace"`
+	SyncInterval        string             `json:"syncInterval"`
 }
 
-// PulseProDeploymentStatus defines the observed state of PulseProDeployment
+type ConfigMapReference struct {
+	Name string `json:"name"`
+	Key  string `json:"key"`
+}
+
+type SecretReference struct {
+	Name      string `json:"name"`
+	ValueFrom string `json:"valueFrom"`
+}
+
 type PulseProDeploymentStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
-// PulseProDeployment is the Schema for the pulseprodeployments API
 type PulseProDeployment struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -52,7 +40,6 @@ type PulseProDeployment struct {
 
 // +kubebuilder:object:root=true
 
-// PulseProDeploymentList contains a list of PulseProDeployment
 type PulseProDeploymentList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
--- internal/controller/pulseprodeployment_controller.go
+++ internal/controller/pulseprodeployment_controller.go
@@ -1,62 +1,99 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package controller
+package controllers
 
 import (
 	"context"
+	"fmt"
+	"os/exec"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	pulseprov1alpha1 "github.com/smarter-contracts/pulsepro-operator/api/v1alpha1"
+	pulseprov1alpha1 "github.com/smarter-contrarcts/pulsepro-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 )
 
-// PulseProDeploymentReconciler reconciles a PulseProDeployment object
 type PulseProDeploymentReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Log logr.Logger
 }
 
-// +kubebuilder:rbac:groups=pulsepro.pulsepro.io,resources=pulseprodeployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=pulsepro.pulsepro.io,resources=pulseprodeployments/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=pulsepro.pulsepro.io,resources=pulseprodeployments/finalizers,verbs=update
+// +kubebuilder:rbac:groups=pulsepro.io,resources=pulseprodeployments,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the PulseProDeployment object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
-func (r *PulseProDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+// Reconcile is the core function that checks the CRD and applies the desired state
+func (r *PulseProDeploymentReconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("pulseprodeployment", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the PulseProDeployment instance
+	instance := &pulseprov1alpha1.PulseProDeployment{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
 
-	return ctrl.Result{}, nil
+	// Fetch ConfigMap for Helm values
+	cm := &corev1.ConfigMap{}
+	err = r.Get(ctx, types.NamespacedName{Name: instance.Spec.HelmValuesConfigMap.Name, Namespace: req.Namespace}, cm)
+	if err != nil {
+		log.Error(err, "Unable to fetch ConfigMap")
+		instance.Status.Status = "Failed to fetch ConfigMap"
+		_ = r.Status().Update(ctx, instance)
+		return reconcile.Result{}, err
+	}
+
+	helmValues := cm.Data[instance.Spec.HelmValuesConfigMap.Key]
+
+	// Check connectivity to external services
+	if err := checkConnectivity(); err != nil {
+		log.Error(err, "Failed to connect to external services")
+		instance.Status.Status = "Failed"
+		_ = r.Status().Update(ctx, instance)
+		return reconcile.Result{}, nil
+	}
+
+	// Run Helm Install/Upgrade
+	if err := runHelmRelease(instance.Spec, helmValues); err != nil {
+		log.Error(err, "Helm release failed")
+		instance.Status.Status = "Failed"
+		_ = r.Status().Update(ctx, instance)
+		return reconcile.Result{}, nil
+	}
+
+	// Update the status
+	instance.Status.Status = "Synced"
+	err = r.Status().Update(ctx, instance)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// Requeue after sync interval for reconciliation
+	syncInterval, _ := time.ParseDuration(instance.Spec.SyncInterval)
+	return reconcile.Result{RequeueAfter: syncInterval}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
-func (r *PulseProDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&pulseprov1alpha1.PulseProDeployment{}).
-		Complete(r)
+// Check connectivity to Vault, MidTier, RabbitMQ, Postgres, TimescaleDB
+func checkConnectivity() error {
+	services := []string{"Vault", "MidTier", "RabbitMQ", "Postgres", "TimescaleDB"}
+
+	for _, service := range services {
+		cmd := exec.Command("ping", "-c", "1", service)
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("failed to connect to %s", service)
+		}
+	}
+	return nil
+}
+
+// runHelmRelease runs the Helm install/upgrade command
+func runHelmRelease(spec pulseprov1alpha1.PulseProDeploymentSpec, values string) error {
+	helmCmd := exec.Command("helm", "upgrade", "--install", spec.PulseProVersion, "--values", values)
+	return helmCmd.Run()
 }
